b3071c76ada9ef2cdf456d4e55a7ed710206e79e
ALi Rezk <133886728+arezk11@users.noreply.github.com>
2024-08-09 16:02:46 +0000

    login error 0

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -13,7 +13,7 @@ const connectDB = async () => {
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  // Add other fields as necessary
+
 });
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
