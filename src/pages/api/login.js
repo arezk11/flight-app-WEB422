17049ed58aa5a82153b2294a32a9f90052d5ede5
Ali Rezk <arezk1@myseneca.ca>
2024-08-11 19:35:33 -0400

    running without server.js 1st try

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -40,3 +40,29 @@
 //     res.status(405).end(`Method ${req.method} Not Allowed`);
 //   }
 // }
+import User from '../../../user-service';
+import jwt from 'jsonwebtoken';
+import connectDB from '../../../utils/connectDB';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  await connectDB();
+
+  try {
+    const user = await User.findOne({ userName: req.body.userName });
+    if (!user || !await user.comparePassword(req.body.password)) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ _id: user._id, userName: user.userName }, JWT_SECRET);
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -38,3 +38,22 @@
 //     res.status(405).end(`Method ${req.method} Not Allowed`);
 //   }
 // }
+import User from '../../../user-service';
+import connectDB from '../../../utils/connectDB';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  await connectDB();
+
+  try {
+    const { fullName, email, phone, userName, password } = req.body;
+    const user = new User({ fullName, emailAddress: email, phoneNo: phone, userName, password });
+    await user.save();
+    res.json({ message: 'Registration successful! Please log in.' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
